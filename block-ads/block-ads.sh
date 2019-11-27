e5105d5e4f07cc91d7506c7f0e1098a37410cb93
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2019-11-27 19:36:35 +0000

    Update block-ads.sh

--- block-ads/block-ads.sh
+++ block-ads/block-ads.sh
@@ -27,11 +27,11 @@ echo -e "\n\n ...initiating LIST var with list of URLs... \n\n"
 LIST="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 https://mirror1.malwaredomains.com/files/justdomains
 http://sysctl.org/cameleon/hosts
-https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
 https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
 https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
 https://hosts-file.net/ad_servers.txt"
 
+## # ZeuS Tracker has been discontinued on Jul 8th, 2019 - https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
 
 #echo ${LIST}
 
